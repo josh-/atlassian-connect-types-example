3454665e3862d6b3bce3b2c3d85d4afe85d566de
Josh Parnham <joshparnham@gmail.com>
2023-04-29 09:48:04 +1000

    Add atlassian-connect example

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    AP.request("/rest/api/latest/thing", {
+      success: function (responseText) {
+        alert(responseText);
+      },
+    });
+  });
+
   return <></>;
 }
